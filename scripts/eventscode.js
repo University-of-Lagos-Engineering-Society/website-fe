49effa4091c4359f6a897dad3f30e0db711d4e43
coded1guy <grggmmnl@gmail.com>
2023-10-07 23:13:32 +0100

    clean: cleaned out the repo

--- scripts/eventscode.js
+++
@@ -1,7 +0,0 @@
-const readMore = document.querySelector("p.read-more");
-const moreEvents = document.querySelector("#more-events");
-
-readMore.addEventListener("click", e => {
-  e.target.style.display = 'none';
-  moreEvents.style.display = 'block';
-});
--- scripts/footer.js
+++
@@ -1,3 +0,0 @@
-// automatically changes the copyright year each new year
-let copyDate = document.querySelector('span.copy-date');
-copyDate.innerText = new Date().getFullYear();
